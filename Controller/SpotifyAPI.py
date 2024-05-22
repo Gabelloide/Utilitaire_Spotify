4ea9223c4e5106dd742c8261ef52099e3009cac6
Olivier <olivier.gabelle1@gmail.com>
2024-05-22 16:34:18 +0200

    Global API instead of Class

--- Controller/SpotifyAPI.py
+++ Controller/SpotifyAPI.py
@@ -1,13 +1,11 @@
 import spotipy
 
-class SpotifyAPI:
+spotify_client = None
 
-  def __init__(self, client_id, client_secret, redirect_uri, scope):
-    self.client_id = client_id
-    self.client_secret = client_secret
-    self.redirect_uri = redirect_uri
-    self.scope = scope
+def setup_client(client_id, client_secret, redirect_uri, scope):
+	global spotify_client
+	spotify_client = spotipy.Spotify(auth_manager=spotipy.SpotifyOAuth(client_id=client_id, client_secret=client_secret, redirect_uri=redirect_uri, scope=scope))
 
-  def get_spotify_client(self):
-    """Gets the Spotify client object with the given credentials and scope."""
-    return spotipy.Spotify(auth_manager=spotipy.SpotifyOAuth(client_id=self.client_id, client_secret=self.client_secret, redirect_uri=self.redirect_uri, scope=self.scope))
+def get_spotify_client():
+	"""Gets the Spotify client object with the given credentials and scope."""
+	return spotify_client
