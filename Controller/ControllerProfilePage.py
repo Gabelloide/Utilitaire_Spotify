248b8197adb0cf7860319884f5cc333cd140661c
Olivier <olivier.gabelle1@gmail.com>
2024-05-26 13:01:37 +0200

    Removed unused methods in ControllerProfilePage

--- Controller/ControllerProfilePage.py
+++ Controller/ControllerProfilePage.py
@@ -22,36 +22,3 @@ class ControllerProfilePage:
 
   def showRecommendations(self):
     pass
-
-
-  def downloadAndSetPicture(self, url, component: QLabel, filename: str):
-    """Using the QNetworkAccessManager to download the profile picture from the URL provided by the Spotify API.
-    If the picture is already in the cache, it is fetched from there. Otherwise, it is downloaded and saved to the cache."""
-    if utils.exists_in_cache(filename):
-      data = utils.load_from_cache(filename)
-      self.fetchFromCache(data, component)
-      return
-
-    request = QNetworkRequest(QUrl(url))
-    reply = self.view.manager.get(request)
-    reply.finished.connect(lambda: self.fetchPicture(reply, component, filename))
-
-
-  def fetchPicture(self, reply, component: QLabel, filename: str):
-    """Triggered when the download of the  picture is finished.
-    Loads the image data into a QPixmap and sets it to the QLabel."""
-    data = reply.readAll()
-    pixmap = QPixmap()
-    pixmap.loadFromData(data)
-    component.setPixmap(pixmap)
-    component.setScaledContents(True)
-    reply.deleteLater()
-    utils.save_to_cache(filename, data)
-
-
-  def fetchFromCache(self, data, component: QLabel):
-    """Fetches the picture from the cache and sets it to the QLabel."""
-    pixmap = QPixmap()
-    pixmap.loadFromData(data)
-    component.setPixmap(pixmap)
-    component.setScaledContents(True)
