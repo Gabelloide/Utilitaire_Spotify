d67eadceebe24719c070cca14bb4fb0a8a3bff08
MrPriingles <baptiste.mrpringles@gmail.com>
2024-05-30 23:18:38 +0200

    Begin Search Page

--- Controller/ControllerSearchPage.py
+++ Controller/ControllerSearchPage.py
@@ -9,3 +9,18 @@ class ControllerSearchPage:
   def __init__(self, user:User, view: SearchPage):
     self.view = view
     self.user = user
+    self.view.searchButton.clicked.connect(lambda : self.search())
+    self.spotify = SpotifyAPI.get_spotify_client()
+    
+  def search(self):
+    search = self.view.searchInput.text()
+  
+    results = self.spotify.search(q=search, limit=20, type='track,artist,album')
+    for idx, track in enumerate(results['tracks']['items']):
+        print(f"{idx+1} - {track['name']} by {track['artists'][0]['name']}")
+
+    
+  
+    
+
+
--- View/SearchPage.py
+++ View/SearchPage.py
@@ -1,5 +1,5 @@
-from PyQt6.QtWidgets import QWidget, QVBoxLayout, QLabel, QHBoxLayout, QSpacerItem, QSizePolicy
-
+from PyQt6.QtWidgets import QWidget, QVBoxLayout, QLabel, QHBoxLayout, QSpacerItem, QSizePolicy, QPushButton, QLineEdit 
+from PyQt6.QtGui import QIcon
 
 class SearchPage(QWidget):
   
@@ -7,10 +7,20 @@ class SearchPage(QWidget):
     super().__init__()
 
     self.parentView = parentView
+    self.searchBar = QHBoxLayout()
+    self.searchInput = QLineEdit()
+    self.searchButton = QPushButton("Rechercher")
     
+    self.searchBar.addWidget(self.searchInput)
+    self.searchBar.addWidget(self.searchButton)
+
     self.mainLayout = QVBoxLayout()
     
-    self.containerTitle = QHBoxLayout()
+    def generateSearchResults(self, results):
+      for idx, track in enumerate(results['tracks']['items']):
+        print(f"{idx+1} - {track['name']} by {track['artists'][0]['name']}")
+    
+    """self.containerTitle = QHBoxLayout()
     self.labelTitle = QLabel("Rechercher")
     
     spacerItem_left = QSpacerItem(40, 20, QSizePolicy.Policy.Expanding, QSizePolicy.Policy.Minimum)
@@ -18,15 +28,15 @@ class SearchPage(QWidget):
     self.containerTitle.addWidget(self.labelTitle)
     spacerItem_right = QSpacerItem(40, 20, QSizePolicy.Policy.Expanding, QSizePolicy.Policy.Minimum)
     self.containerTitle.addItem(spacerItem_right)
-    
+    """
     
     # CSS
     with open("Assets/style.css", "r") as file:
       stylesheet = file.read()
 
-    self.labelTitle.setStyleSheet(stylesheet)
+    #self.labelTitle.setStyleSheet(stylesheet)
     
     
     
-    self.mainLayout.addLayout(self.containerTitle)
+    self.mainLayout.addLayout(self.searchBar)
     self.setLayout(self.mainLayout)
