0641eea6e90e96973c4dacdb06aeea22b4fd0b7f
Olivier <olivier.gabelle1@gmail.com>
2024-06-01 16:53:04 +0200

    Style for search bar

--- View/SearchPage.py
+++ View/SearchPage.py
@@ -1,4 +1,6 @@
-from PyQt6.QtWidgets import QWidget, QVBoxLayout, QLabel, QHBoxLayout, QSpacerItem, QSizePolicy, QPushButton, QLineEdit 
+from PyQt6.QtWidgets import QWidget, QVBoxLayout, QLabel, QHBoxLayout, QSpacerItem, QSizePolicy, QPushButton, QLineEdit
+from PyQt6.QtCore import Qt
+from PyQt6.QtGui import QPixmap
 from PyQt6.QtGui import QIcon
 
 class SearchPage(QWidget):
@@ -7,14 +9,54 @@ class SearchPage(QWidget):
     super().__init__()
 
     self.parentView = parentView
+
+    # Créer la barre de recherche
     self.searchBar = QHBoxLayout()
+
+    # Créer un widget conteneur pour encapsuler l'icône et le champ de recherche
+    searchContainer = QWidget()
+    searchContainerLayout = QHBoxLayout(searchContainer)
+
+    # Créer le champ de texte de recherche
     self.searchInput = QLineEdit()
-    self.searchButton = QPushButton("Rechercher")
+    self.searchInput.setPlaceholderText("Que souhaitez-vous chercher ?")
     
-    self.searchBar.addWidget(self.searchInput)
-    self.searchBar.addWidget(self.searchButton)
+    # Appliquer le style au widget conteneur
+    searchContainer.setStyleSheet("""
+        QWidget {
+            border: 2px solid gray;
+            border-radius: 20px;
+            padding: 15px;
+        }
+        QLineEdit {
+            border: none;
+            border-radius: 0px;
+            padding: 5px;
+            margin: 5px;
+            font-size: 14pt;
+        }
+        QLabel {
+          border: none;
+          background-color: transparent;
+        }
+    """)
+
+    # Ajouter l'icône de recherche
+    searchIcon = QLabel()
+    searchIcon.setPixmap(QPixmap('Assets/icons/search_icon.png').scaled(25, 25, Qt.AspectRatioMode.KeepAspectRatio, Qt.TransformationMode.SmoothTransformation))
+
+    # Ajouter l'icône et le champ de texte au layout du widget conteneur
+    searchContainerLayout.addWidget(searchIcon)
+    searchContainerLayout.addWidget(self.searchInput)
+    searchContainerLayout.setContentsMargins(0, 0, 0, 0)
+    searchContainerLayout.setSpacing(5)
+    
+    # Ajouter le widget conteneur à la barre de recherche
+    self.searchBar.addWidget(searchContainer)
+    self.searchBar.setAlignment(Qt.AlignmentFlag.AlignLeft)
+
+    # Créer le layout principal
     self.results = QVBoxLayout()
-    
     self.mainLayout = QVBoxLayout()
     self.mainLayout.addLayout(self.searchBar)
     self.mainLayout.addLayout(self.results)
@@ -24,10 +66,3 @@ class SearchPage(QWidget):
     self.mainLayout.addItem(spacer)
     
     self.setLayout(self.mainLayout)
-
-    
-    # CSS
-    with open("Assets/style.css", "r") as file:
-      stylesheet = file.read()
-
-   
